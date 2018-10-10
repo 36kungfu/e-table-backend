0c0c481b1eb50f678487df65a9443dce34e8eeb5
Edkung <36kungfu@gmail.com>
2018-10-10 21:35:12 +0700

    Access-Control-Allow-Origin: *

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const Express = require('express')
 const routes = require('./routes')
 
 const app = Express()
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
+  next()
+})
 app.use(BodyParser.json())
 app.use(BodyParser.urlencoded({ extended: true }))
 app.use('/', routes)
