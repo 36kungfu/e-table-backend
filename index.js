e016a2dacabaf48f0ba75ebb1707e619f9ec21a8
Edkung <36kungfu@gmail.com>
2018-10-11 13:04:37 +0700

    test add response headers

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
   // res.header('Access-Control-Allow-Headers', 'Content-Type')
-  res.header('Access-Control-Allow-Headers', 'Content-Type, x-total-count')
+  res.header('Access-Control-Allow-Headers', ['Content-Type', 'x-total-count'])
   // res.header('Access-Control-Allow-Credentials', true)
   // res.header('Access-Control-Expose-Headers', 'x-total-count')
   next()
