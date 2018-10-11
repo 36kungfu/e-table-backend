19237ad7e73e54c6fe8aea96d4994ac544c3d45a
Edkung <36kungfu@gmail.com>
2018-10-11 13:14:05 +0700

    test set response headers

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Headers', ['Content-Type', 'x-total-count'])
   // res.header('Access-Control-Allow-Credentials', true)
   // res.header('Access-Control-Expose-Headers', 'x-total-count')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-total-count'
+  )
   next()
 })
 app.use(BodyParser.json())
