72b0ff260e333637c16195b86248d5a45e295c8f
Edkung <36kungfu@gmail.com>
2018-10-11 13:17:11 +0700

    remove allowed headers

--- index.js
+++ index.js
@@ -8,14 +8,7 @@ const app = Express()
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS')
-  // res.header('Access-Control-Allow-Headers', 'Content-Type')
-  res.header('Access-Control-Allow-Headers', ['Content-Type', 'x-total-count'])
-  // res.header('Access-Control-Allow-Credentials', true)
-  // res.header('Access-Control-Expose-Headers', 'x-total-count')
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization, x-total-count'
-  )
+  res.header('Access-Control-Allow-Headers', 'Content-Type')
   next()
 })
 app.use(BodyParser.json())
